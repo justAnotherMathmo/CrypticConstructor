7ddaf33bcf3c999a1c3f85a46914247147d41fde
Kaimyn Chapman <kchap21@hotmail.com>
2018-05-05 00:45:41 +0100

    Command line argument passed as int

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import constructor
 import drawer
 
 if __name__ == '__main__':
-    grid_size = sys.argv[1]  # 13
+    grid_size = int(sys.argv[1])  # 13
     url = sys.argv[2]  # r'https://times-xwd-times.livejournal.com/1935273.html'
     across_clues, down_clues = collector.get_parsed_clues(url)
 
