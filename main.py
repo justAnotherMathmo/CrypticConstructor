91429fb958d1ff7ceac44a73a87d078471addf34
Kaimyn Chapman <kchap21@hotmail.com>
2018-05-05 00:44:24 +0100

    Command line arguments

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import constructor
 import drawer
 
 if __name__ == '__main__':
-    grid_size = 13 # sys.argv[1]
-    url = r'https://times-xwd-times.livejournal.com/1935273.html' # sys.argv[2]
+    grid_size = sys.argv[1]  # 13
+    url = sys.argv[2]  # r'https://times-xwd-times.livejournal.com/1935273.html'
     across_clues, down_clues = collector.get_parsed_clues(url)
 
     num_grid, word_grid = constructor.constructor(grid_size, across_clues, down_clues)
