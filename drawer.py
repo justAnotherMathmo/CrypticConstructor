fddc78189ad7d93ecee5872041b0f2b3f87be08a
Kaimyn Chapman <kchap21@hotmail.com>
2018-05-05 10:15:42 +0100

    Started added proper drawing

--- drawer.py
+++ drawer.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import turtle
 
 vector_len = np.vectorize(len)
 ascii_empty =  '..'
@@ -12,7 +12,7 @@ def solution_drawer(word_grid):
     return wgrid
 
 
-def drawer(num_grid, word_grid, across_clues, down_clues):
+def ascii_drawer(num_grid, word_grid, across_clues, down_clues):
     wgrid = word_grid.astype('<U2')
 
     # First fill all empty strings with dashes
@@ -26,3 +26,24 @@ def drawer(num_grid, word_grid, across_clues, down_clues):
     wgrid[vector_len(wgrid) < 2] = ['0' + str(i) for i in range(1, 10)]
 
     return wgrid
+
+
+def draw_box(t, x, y, size, fill_color):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+
+    t.fillcolor(fill_color)
+    t.begin_fill()  # Shape drawn after this will be filled with this color!
+
+    for i in range(0, 4):
+        t.forward(size) # move forward
+        t.right(90) # turn pen right 90 degrees
+
+    t.end_fill() # Go ahead and fill the rectangle!
+
+
+def drawer(num_grid, word_grid, across_clues, down_clues):
+    # print(num_grid)
+    # return solution_drawer(word_grid)
+    return ascii_drawer(num_grid, word_grid, across_clues, down_clues)
